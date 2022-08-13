7030492658659b9e1a35610c0c3c7b2b57548f81
Sereyvathanass <110382674+Sereyvathanass@users.noreply.github.com>
2022-08-13 16:23:08 +0700

    iwinsalot

---
+++ index.js
@@ -0,0 +1,162 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  function slot(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('home1').style.display="none";
+    document.getElementById('slot').style.display="inline";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function tablegame(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="inline";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function live(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="inline";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function fishing(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="inline";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function tournament(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="inline";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+
+  }
+  function sport(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="inline";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function casino(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="inline";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function other(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="inline";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function alls(){
+    document.getElementById('home1').style.display="inline";
+    document.getElementById('home2').style.display="inline";
+    document.getElementById('slot').style.display="inline";
+    document.getElementById('tablegame').style.display="inline";
+    document.getElementById('live').style.display="inline";
+    document.getElementById('fishing').style.display="inline";
+    document.getElementById('tournament').style.display="inline";
+    document.getElementById('sport').style.display="inline";
+    document.getElementById('casino').style.display="inline";
+    document.getElementById('other').style.display="inline";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="none";
+  }
+  function popular(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="inline";
+    document.getElementById('new1').style.display="none";
+  }
+  function new1(){
+    document.getElementById('home1').style.display="none";
+    document.getElementById('home2').style.display="none";
+    document.getElementById('slot').style.display="none";
+    document.getElementById('tablegame').style.display="none";
+    document.getElementById('live').style.display="none";
+    document.getElementById('fishing').style.display="none";
+    document.getElementById('tournament').style.display="none";
+    document.getElementById('sport').style.display="none";
+    document.getElementById('casino').style.display="none";
+    document.getElementById('other').style.display="none";
+    document.getElementById('popular').style.display="none";
+    document.getElementById('new1').style.display="inline";
+  }
